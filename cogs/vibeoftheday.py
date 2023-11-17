b6d1f429819843f72085a8ce14df6a8d9a828947
Binary12Spy <ethand525@gmail.com>
2023-11-17 17:04:48 -0500

    Added vibe cahcing to external file to persist between reloads/restarts

--- cogs/vibeoftheday.py
+++ cogs/vibeoftheday.py
@@ -1,3 +1,4 @@
+import json
 import os
 import random
 import spotipy
@@ -10,21 +11,38 @@ from dotenv import load_dotenv
 from nextcord.ext import commands
 from spotipy.oauth2 import SpotifyClientCredentials
 
+CACHE_DIR = "./cogs/.cache/vibeoftheday.cache"
+
 class VibeOfTheDay(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-    
     load_dotenv()
     SPOTIFY_APP_CLIENT_ID = os.environ.get("SPOTIFY_APP_CLIENT_ID")
     SPOTIFY_APP_CLIENT_SECRET = os.environ.get("SPOTIFY_APP_CLIENT_SECRET")
     SPOTIFY_VIBE_PLAYLIST_ID = os.environ.get("SPOTIFY_VIBE_PLAYLIST_ID")
     VIBE_ANNOUNCEMENT_CHANNEL = os.environ.get("VIBE_ANNOUNCEMENT_CHANNEL")
     
-    current_vibe_spot_url = ""
-    current_vibe_name = ""
-    current_vibe_artists = ""
-    current_vibe_ytm_url = ""
+    def __init__(self, bot):
+        self.bot = bot
+        # Check if the file exists
+        if not os.path.exists(CACHE_DIR):
+            # Create the file
+            with open(CACHE_DIR, 'w') as file:
+                file.write('')  # You can write initial content if needed
+                
+        self.cache_file = open(CACHE_DIR, "r+")
+        self.load_from_cache()
+        
+    def load_from_cache(self):
+        string = self.cache_file.read()
+        if string == '':
+            self.current_vibe = {"name": "", "artists": "", "urls": {"spotify": "", "yt_music": ""}}
+            self.get_a_vibe()
+            return
+        self.current_vibe = json.loads(string)
     
+    def save_to_cache(self):
+        self.cache_file.write(json.dumps(self.current_vibe))
+    
+    current_vibe = {}
     sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=SPOTIFY_APP_CLIENT_ID, client_secret=SPOTIFY_APP_CLIENT_SECRET))
     
     def get_a_vibe(self):
@@ -39,24 +57,26 @@ class VibeOfTheDay(commands.Cog):
         for artist in track["artists"]:
             artists.append(artist["name"])
         
-        self.current_vibe_name = track["name"]
-        self.current_vibe_spot_url = track["external_urls"]["spotify"]
-        self.current_vibe_artists = ", ".join(artists)
-        self.youtube_music_url()
+        self.current_vibe["name"] = track["name"]
+        self.current_vibe["urls"]["spotify"] = track["external_urls"]["spotify"]
+        self.current_vibe["artists"] = ", ".join(artists)
+        self.current_vibe["urls"]["yt_music"] = self.youtube_music_url()
+        self.save_to_cache()
     
     def youtube_music_url(self):
-        ytm_id = YouTubeMusicAPI.search(f"{self.current_vibe_name} by {self.current_vibe_artists}")["id"]
-        self.current_vibe_ytm_url = f"https://music.youtube.com/watch?v={ytm_id}"
+        ytm_id = YouTubeMusicAPI.search(f"{self.current_vibe["name"]} by {self.current_vibe["artists"]}")["id"]
+        return f"https://music.youtube.com/watch?v={ytm_id}"
     
     def vibe_message(self):
         message = dedent(f"""
             ### Vibe of the day:
-            *{self.current_vibe_name}* by *{self.current_vibe_artists}*
-            [Spotify]({self.current_vibe_spot_url}) - [YT Music]({self.current_vibe_ytm_url})
+            *{self.current_vibe["name"]}* by *{self.current_vibe["artists"]}*
+            [Spotify]({self.current_vibe["urls"]["spotify"]}) - [YT Music]({self.current_vibe["urls"]["yt_music"]})
             """)
         return message
     
     def cog_unload(self):
+        self.cache_file.close()
         self.background_task.cancel()
 
     @tasks.loop(hours=1.0)
@@ -73,6 +93,4 @@ class VibeOfTheDay(commands.Cog):
         self.background_task.start()
 
 def setup(bot):
-    cog = VibeOfTheDay(bot)
-    cog.get_a_vibe()
-    bot.add_cog(cog)
+    bot.add_cog(VibeOfTheDay(bot))
